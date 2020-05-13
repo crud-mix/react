7e447d4466fb3f4c338ea578139d9b6ff09d69ea
Jewel Jacob <jeweljacob387@gmail.com>
2020-05-13 23:17:47 +0530

    added sample for function component

---
+++ basic-react/src/FunctionComponent/FunctionComponent.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default class FunctionComponent extends React.Component {
+    data = {
+        name: 'John Doe',
+        age: -99
+    }
+
+    render() {
+        return <SimpleComponent data={this.data} />
+    }
+
+}
+
+function SimpleComponent(props) {
+    return (
+        <div>
+            <h3>Function Component</h3>
+            <p>The simplest way to define a component is to write a JavaScript functio</p>
+            <p>
+                <span>{props.data.name}</span> is <span>{props.data.age} year(s) old</span>
+            </p>
+        </div>
+    )
+}
